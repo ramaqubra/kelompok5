42ccd7be51128b44083d15ab6e2c3c4a1b883917
Ayustina22006 <ayustinasamudin22006@gmail.com>
2023-11-24 02:33:01 +0800

    Tampil Data & Setting Admin

--- Env/Project/AplikasiLayananUMKM/models.py
+++ Env/Project/AplikasiLayananUMKM/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    nama_menu = models.CharField(max_length=255)
+    deskripsi = models.TextField()
+    harga = models.DecimalField(max_digits=10, decimal_places=2)
+    gambar = models.ImageField(upload_to='static/assets/imgs/', null=True)
+
+    def __str__(self):
+        return f"{self.nama_menu}"
--- Env/Project/AplikasiLayananUMKM/views.py
+++ Env/Project/AplikasiLayananUMKM/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Menu
 
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
 def details(request):
+    menu = Menu.objects.all()
     template = loader.get_template('details.html')
-    return HttpResponse(template.render())
+    context = {
+        'menu':menu
+    }
+    return HttpResponse(template.render(context,request))
