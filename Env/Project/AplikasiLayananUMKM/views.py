f749cf671308036b1ac27214ad2e9f52c42e118d
Farras10 <farrassida@gmail.com>
2023-11-23 21:32:08 +0800

    Modifikasi Tampilan Katalog

--- Env/Project/AplikasiLayananUMKM/views.py
+++ Env/Project/AplikasiLayananUMKM/views.py
@@ -6,3 +6,6 @@ def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+def details(request):
+    template = loader.get_template('details.html')
+    return HttpResponse(template.render())
