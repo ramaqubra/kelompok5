c010c053a2d0cd9d6e58b079013d945aa5aa4a51
ikhwanz29 <ikhwanuluzlahtkj@gmail.com>
2023-11-29 05:45:46 +0800

    Setting Upload Image

--- Env/Project/AplikasiLayananUMKM/models.py
+++ Env/Project/AplikasiLayananUMKM/models.py
@@ -4,7 +4,7 @@ class Menu(models.Model):
     nama_menu = models.CharField(max_length=255)
     deskripsi = models.TextField()
     harga = models.DecimalField(max_digits=10, decimal_places=2)
-    gambar = models.ImageField(upload_to='static/assets/imgs/', null=True)
+    gambar = models.ImageField(upload_to='img/', null=True)
 
     def __str__(self):
         return f"{self.nama_menu}"
--- db_kelompok5_pbo.sql
+++ db_kelompok5_pbo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2023 at 02:15 AM
+-- Generation Time: Nov 28, 2023 at 10:42 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -40,9 +40,9 @@ CREATE TABLE `aplikasilayananumkm_menu` (
 --
 
 INSERT INTO `aplikasilayananumkm_menu` (`id`, `nama_menu`, `deskripsi`, `harga`, `gambar`) VALUES
-(1, 'Roti Bakarta Cokelat', 'Roti bakar cokelat adalah varian roti yang dicelupkan atau dilapisi dengan cokelat, memberikan rasa manis dan gurih pada roti. Biasanya, roti bakar cokelat memiliki lapisan cokelat yang meleleh dan memberikan pengalaman rasa yang memuaskan.', '28000.00', 'static/assets/imgs/bakartacoklat_1_wXe3Yvz.jpeg'),
-(2, 'Roti Bakarta Green Tea', 'Roti bakar greentea adalah roti yang diolah dengan tambahan bubuk teh hijau, memberikan rasa dan aroma khas teh hijau. Roti ini seringkali memiliki warna hijau yang lembut dan memberikan sentuhan unik dari teh hijau, yang bisa memberikan pengalaman rasa yang segar dan sedikit pahit.', '30000.00', 'static/assets/imgs/bakartagreentea_1_8fV1R2s.jpeg'),
-(3, 'Roti Bakarta Keju', 'Roti bakar keju adalah varian roti yang biasanya dicampur atau dilapisi dengan keju, menciptakan kombinasi rasa gurih dan creamy. Keju yang meleleh pada roti bakar memberikan tambahan kelezatan dan membuatnya menjadi pilihan yang populer di kalangan pecinta keju.', '26000.00', 'static/assets/imgs/bakartakeju_1_P5ORHdQ.jpeg');
+(12, 'Roti Bakarta Cokelat', 'Roti bakar cokelat adalah varian roti yang dicelupkan atau dilapisi dengan cokelat, memberikan rasa manis dan gurih pada roti. Biasanya, roti bakar cokelat memiliki lapisan cokelat yang meleleh dan memberikan pengalaman rasa yang memuaskan.', '28000.00', 'img/bakartacoklat_1.jpeg'),
+(13, 'Roti Bakarta Green Tea', 'Roti bakar greentea adalah roti yang diolah dengan tambahan bubuk teh hijau, memberikan rasa dan aroma khas teh hijau. Roti ini seringkali memiliki warna hijau yang lembut dan memberikan sentuhan unik dari teh hijau, yang bisa memberikan pengalaman rasa yang segar dan sedikit pahit.', '30000.00', 'img/bakartagreentea_1.jpeg'),
+(14, 'Roti Bakarta Keju', 'Roti bakar keju adalah varian roti yang biasanya dicampur atau dilapisi dengan keju, menciptakan kombinasi rasa gurih dan creamy. Keju yang meleleh pada roti bakar memberikan tambahan kelezatan dan membuatnya menjadi pilihan yang populer di kalangan pecinta keju.', '26000.00', 'img/bakartakeju_1.jpeg');
 
 -- --------------------------------------------------------
 
@@ -139,7 +139,7 @@ CREATE TABLE `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
-(1, 'pbkdf2_sha256$600000$X3MCEoyLjmCiJAFlns9Ml3$G36PTwKW0U4DbdXEWp0f2HesZiCCLhimgggJKMlvyQs=', '2023-11-23 22:37:58.839924', 1, 'admin', '', '', 'ikhwanuluzlahtkj@gmail.com', 1, 1, '2023-11-22 23:45:45.047875');
+(1, 'pbkdf2_sha256$600000$X3MCEoyLjmCiJAFlns9Ml3$G36PTwKW0U4DbdXEWp0f2HesZiCCLhimgggJKMlvyQs=', '2023-11-27 10:57:01.990124', 1, 'admin', '', '', 'ikhwanuluzlahtkj@gmail.com', 1, 1, '2023-11-22 23:45:45.047875');
 
 -- --------------------------------------------------------
 
@@ -189,7 +189,21 @@ CREATE TABLE `django_admin_log` (
 INSERT INTO `django_admin_log` (`id`, `action_time`, `object_id`, `object_repr`, `action_flag`, `change_message`, `content_type_id`, `user_id`) VALUES
 (1, '2023-11-24 01:12:05.630038', '1', 'Roti Bakarta Cokelat', 1, '[{\"added\": {}}]', 7, 1),
 (2, '2023-11-24 01:13:21.674472', '2', 'Roti Bakarta Green Tea', 1, '[{\"added\": {}}]', 7, 1),
-(3, '2023-11-24 01:13:58.080732', '3', 'Roti Bakarta Keju', 1, '[{\"added\": {}}]', 7, 1);
+(3, '2023-11-24 01:13:58.080732', '3', 'Roti Bakarta Keju', 1, '[{\"added\": {}}]', 7, 1),
+(4, '2023-11-27 10:30:33.446681', '4', 'Roti', 1, '[{\"added\": {}}]', 7, 1),
+(5, '2023-11-27 10:33:38.975969', '5', 'Tes', 1, '[{\"added\": {}}]', 7, 1),
+(6, '2023-11-27 10:41:18.704557', '6', 'Roti Lagi', 1, '[{\"added\": {}}]', 7, 1),
+(7, '2023-11-27 10:45:36.471407', '7', 'Tes', 1, '[{\"added\": {}}]', 7, 1),
+(8, '2023-11-27 10:52:55.358613', '8', 'TES', 1, '[{\"added\": {}}]', 7, 1),
+(9, '2023-11-27 10:55:25.988304', '9', 'Tampil', 1, '[{\"added\": {}}]', 7, 1),
+(10, '2023-11-27 10:57:27.408183', '10', '((((9999', 1, '[{\"added\": {}}]', 7, 1),
+(11, '2023-11-27 11:02:33.960789', '11', 'TES', 1, '[{\"added\": {}}]', 7, 1),
+(12, '2023-11-28 21:33:18.870690', '3', 'Roti Bakarta Keju', 3, '', 7, 1),
+(13, '2023-11-28 21:33:18.916704', '2', 'Roti Bakarta Green Tea', 3, '', 7, 1),
+(14, '2023-11-28 21:33:18.920692', '1', 'Roti Bakarta Cokelat', 3, '', 7, 1),
+(15, '2023-11-28 21:39:34.226515', '12', 'Roti Bakarta Cokelat', 1, '[{\"added\": {}}]', 7, 1),
+(16, '2023-11-28 21:39:55.064749', '13', 'Roti Bakarta Green Tea', 1, '[{\"added\": {}}]', 7, 1),
+(17, '2023-11-28 21:40:14.524618', '14', 'Roti Bakarta Keju', 1, '[{\"added\": {}}]', 7, 1);
 
 -- --------------------------------------------------------
 
@@ -253,7 +267,9 @@ INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
 (17, 'auth', '0012_alter_user_first_name_max_length', '2023-11-22 23:42:30.643265'),
 (18, 'sessions', '0001_initial', '2023-11-22 23:42:30.705283'),
 (19, 'AplikasiLayananUMKM', '0001_initial', '2023-11-23 22:14:33.101772'),
-(20, 'AplikasiLayananUMKM', '0002_menu_gambar', '2023-11-23 22:14:33.138763');
+(20, 'AplikasiLayananUMKM', '0002_menu_gambar', '2023-11-23 22:14:33.138763'),
+(21, 'AplikasiLayananUMKM', '0003_alter_menu_gambar', '2023-11-27 10:44:44.951780'),
+(22, 'AplikasiLayananUMKM', '0004_alter_menu_gambar', '2023-11-27 10:54:33.947477');
 
 -- --------------------------------------------------------
 
@@ -273,7 +289,7 @@ CREATE TABLE `django_session` (
 
 INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
 ('ssv9msk50sgs0248ovm7fi49zsowrkpj', '.eJxVjDEOAiEURO9CbciHhQUs7T0D-cBHVg0ky25lvLuSbKHNFPPezIt53Lfi906rXxI7M8FOv13A-KA6QLpjvTUeW93WJfCh8IN2fm2JnpfD_Tso2Mt3jaQAEEBaMWVLs1FxhMxKg0JnaNIWgjM2R3LgMOYQpEbh5JTlrCN7fwDKrjdz:1r6HzD:PGgwF7mH--621a7MlzFdj8k1TE14rW1EBxhB5dbyEGo', '2023-12-07 22:15:51.495629'),
-('yo3skz81q4pwkhpzw4gtzixv55bkximf', '.eJxVjDEOAiEURO9CbciHhQUs7T0D-cBHVg0ky25lvLuSbKHNFPPezIt53Lfi906rXxI7M8FOv13A-KA6QLpjvTUeW93WJfCh8IN2fm2JnpfD_Tso2Mt3jaQAEEBaMWVLs1FxhMxKg0JnaNIWgjM2R3LgMOYQpEbh5JTlrCN7fwDKrjdz:1r6IKc:LurLuPINHO9lneBmxOl5oHKBeM_8Frem8EaHrIkUc8Y', '2023-12-07 22:37:58.856930');
+('wh99bpn5fdqv8ng6fia3j9kps9taunr6', '.eJxVjDEOAiEURO9CbciHhQUs7T0D-cBHVg0ky25lvLuSbKHNFPPezIt53Lfi906rXxI7M8FOv13A-KA6QLpjvTUeW93WJfCh8IN2fm2JnpfD_Tso2Mt3jaQAEEBaMWVLs1FxhMxKg0JnaNIWgjM2R3LgMOYQpEbh5JTlrCN7fwDKrjdz:1r7ZIU:LVEdLr_3kCxabNhI0u2s9Kzmq0gnDTOOsHtK3LwkIEQ', '2023-12-11 10:57:02.027128');
 
 --
 -- Indexes for dumped tables
@@ -366,7 +382,7 @@ ALTER TABLE `django_session`
 -- AUTO_INCREMENT for table `aplikasilayananumkm_menu`
 --
 ALTER TABLE `aplikasilayananumkm_menu`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `auth_group`
@@ -408,7 +424,7 @@ ALTER TABLE `auth_user_user_permissions`
 -- AUTO_INCREMENT for table `django_admin_log`
 --
 ALTER TABLE `django_admin_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `django_content_type`
@@ -420,7 +436,7 @@ ALTER TABLE `django_content_type`
 -- AUTO_INCREMENT for table `django_migrations`
 --
 ALTER TABLE `django_migrations`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
