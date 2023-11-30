2aac9d0baeced3fb9c285310182f7ce4c415c7c2
nurendro <nurendro.47@gmail.com>
2023-11-30 17:31:19 +0800

    import fungsi dari views.py ke urls.py

--- Env/Project/AplikasiLayananUMKM/urls.py
+++ Env/Project/AplikasiLayananUMKM/urls.py
@@ -1,5 +1,6 @@
+# Nurendro Hardjo_E1E122133
 from django.urls import path
-from . import views
+from .views import index, menu, aishtea, saguku, wang
 
 urlpatterns = [
     path('', views.index, name="index"),
