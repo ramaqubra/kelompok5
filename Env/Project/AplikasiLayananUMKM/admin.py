6e0c195a8d1b9b6cacaa90407726785a6ba83d07
RaihanPutra324 <muhraihanputrakhamil@gmail.com>
2023-11-30 16:15:22 +0800

    Lanjutan Register Model

--- Env/Project/AplikasiLayananUMKM/admin.py
+++ Env/Project/AplikasiLayananUMKM/admin.py
@@ -8,3 +8,12 @@ class Menu(admin.ModelAdmin):
 @admin.register(AishTea)
 class AishTeaAdmin(admin.ModelAdmin):
     list_display = ['nama_menu', 'harga']
+    
+# E1E122070_MUHAMMAD RAIHAN PUTRA KHAMIL
+@admin.register(Saguku)
+class SagukuAdmin(admin.ModelAdmin):
+    list_display = ['nama_menu', 'harga']
+
+@admin.register(Wang)
+class WangAdmin(admin.ModelAdmin):
+    list_display = ['nama_menu', 'harga']
