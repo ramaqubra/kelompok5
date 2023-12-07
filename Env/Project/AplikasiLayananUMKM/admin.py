19fb1d0872a5a0fde7ab02adc116fdf63d6cec02
ikhwanz29 <ikhwanuluzlahtkj@gmail.com>
2023-12-07 21:09:16 +0800

    Setting penampilan gambar di admin

--- Env/Project/AplikasiLayananUMKM/admin.py
+++ Env/Project/AplikasiLayananUMKM/admin.py
@@ -1,21 +1,52 @@
 from django.contrib import admin
+from django.utils.html import format_html
 # E1E122025_ NUR FADILLAH
 from .models import Roti, AishTea, Saguku, Wang
 
-# Uzlah merubah import dan register Menu menjadi Roti dan RotiAdmin 
-@admin.register(Roti)
-class RotiAdmin(admin.ModelAdmin):
-    list_display = ['nama_menu', 'harga']
+# Uzlah merubah import dan register Menu menjadi Roti dan RotiAdmin sekaligus setting untuk tampilkan gambar di admin
+# @admin.register(Roti)
+# class RotiAdmin(admin.ModelAdmin):
+#     list_display = ['nama_menu', 'deskripsi', 'harga', 'display_image']
 
-@admin.register(AishTea)
-class AishTeaAdmin(admin.ModelAdmin):
-    list_display = ['nama_menu', 'harga']
+#     def display_image(self,obj):
+#         return format_html('<img src="{}" width=100 />',obj.gambar.url)
+
+#     display_image.short_description="Gambar"
+
+# @admin.register(AishTea)
+# class AishTeaAdmin(admin.ModelAdmin):
+#     list_display = ['nama_menu', 'deskripsi', 'harga', 'display_image']
+
+#     def display_image(self,obj):
+#         return format_html('<img src="{}" width=100 />',obj.gambar.url)
+
+#     display_image.short_description="Gambar"
     
-# E1E122070_MUHAMMAD RAIHAN PUTRA KHAMIL
-@admin.register(Saguku)
-class SagukuAdmin(admin.ModelAdmin):
-    list_display = ['nama_menu', 'harga']
+# # E1E122070_MUHAMMAD RAIHAN PUTRA KHAMIL
+# @admin.register(Saguku)
+# class SagukuAdmin(admin.ModelAdmin):
+#     list_display = ['nama_menu', 'deskripsi', 'harga', 'display_image']
 
-@admin.register(Wang)
-class WangAdmin(admin.ModelAdmin):
-    list_display = ['nama_menu', 'harga']
+#     def display_image(self,obj):
+#         return format_html('<img src="{}" width=100 />',obj.gambar.url)
+
+#     display_image.short_description="Gambar"
+
+# @admin.register(Wang)
+# class WangAdmin(admin.ModelAdmin):
+#     list_display = ['nama_menu', 'deskripsi', 'harga', 'display_image']
+
+#     def display_image(self,obj):
+#         return format_html('<img src="{}" width=100 />',obj.gambar.url)
+
+#     display_image.short_description="Gambar"
+
+# Agar syntaxnya menjadi lebih singkat
+@admin.register(Roti, AishTea, Saguku, Wang)
+class BaseMenuAdmin(admin.ModelAdmin):
+    list_display = ["nama_menu","deskripsi","harga","display_image"]
+
+    def display_image(self,obj):
+        return format_html('<img src="{}" width=100 />',obj.gambar.url)
+
+    display_image.short_description="Gambar"
